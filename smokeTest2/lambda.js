7b78fd570a81e6f4eef816cf6715e90f75c8c325
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-06-12 12:22:08 +0530

    Add an api gateway and sns

--- smokeTest2/lambda.js
+++ smokeTest2/lambda.js
@@ -1,10 +1,24 @@
 let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
 const ses = new AWS.SES();
 const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
 
 	console.log("S3 as a trigger");
 
+	sns.publish({
+		Message: 'Smoke test fr Sigma',
+		Subject: 'Smoke Test',
+		MessageAttributes: {},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:318300609668:smokeTest'
+	}).promise()
+		.then(data => {
+			// your code goes here
+		})
+		.catch(err => {
+			// error handling goes here
+		});
 
 
 	callback(null, 'Successfully executed');
