874a58e7de2833a4bdf5c962cc0dddd705d0b902
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-06-12 12:08:45 +0530

    add a ses as a resource

--- smokeTest2/lambda.js
+++ smokeTest2/lambda.js
@@ -1,4 +1,5 @@
 let AWS = require('aws-sdk');
+const ses = new AWS.SES();
 const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
 
@@ -14,6 +15,29 @@ exports.handler = function (event, context, callback) {
 		.catch(err => {
 			console.log(err, err.stack); // an error occurred
 		});
+
+	ses.sendEmail({
+		Destination: {
+			ToAddresses: ['kannangarar1993@gmail.com'],
+			CcAddresses: [],
+			BccAddresses: []
+		},
+		Message: {
+			Body: {
+				Text: {
+					Data: ''
+				}
+			},
+			Subject: {
+				Data: 'SmokeTest'
+			}
+		},
+		Source: 'andun@adroitlogic.com',
+	}, function (err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else console.log(data);           // successful response
+	});
+
 
 	callback(null, 'Successfully executed');
 
