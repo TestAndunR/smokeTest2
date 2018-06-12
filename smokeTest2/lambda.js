dba14c408c7911c3f74e4a019155e1def8880488
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-06-12 11:33:11 +0530

    add an cloud watch with scheduler

--- smokeTest2/lambda.js
+++ smokeTest2/lambda.js
@@ -1,28 +1,7 @@
 let AWS = require('aws-sdk');
-let SL_AWS = require('slappforge-sdk-aws');
-let connectionManager = require('./ConnectionManager');
-const rds = new SL_AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
 
-	// You can pass the existing connection to this function.
-	// A new connection will be created if it's not present as the third param 
-	// You must always end/destroy the DB connection after it's used
-	rds.query({
-		instanceIdentifier: 'smokeTest',
-		query: 'INSERT INTO tests(Testno)VALUES("01")',
-		inserts: []
-	}, function (error, results, connection) {
-		if (error) {
-			console.log("Error occurred");
-			throw error;
-		} else {
-			console.log("Success")
-			console.log(results);
-		}
-
-		connection.end();
-	});
-
+	console.log("Schedule rule");
 
 	callback(null, 'Successfully executed');
 }
