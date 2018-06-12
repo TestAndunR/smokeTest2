8f74824fb225f380b39702a1d79f5b28a2a5cc63
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-06-12 11:56:02 +0530

    add a s3 a trigger

--- smokeTest2/lambda.js
+++ smokeTest2/lambda.js
@@ -1,8 +1,8 @@
 let AWS = require('aws-sdk');
 exports.handler = function (event, context, callback) {
 
-	console.log("Schedule rule");
+	console.log("S3 as a trigger");
 
 	callback(null, 'Successfully executed');
-	
+
 }
