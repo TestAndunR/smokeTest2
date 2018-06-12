fc65bbcb21029c8d90ec939dd876454da00754fe
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-06-12 12:05:07 +0530

    add a s3 a resource with list object

--- smokeTest2/lambda.js
+++ smokeTest2/lambda.js
@@ -1,7 +1,19 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
 
 	console.log("S3 as a trigger");
+	s3.listObjects({
+		'Bucket': 'sigma.smoketest',
+		'MaxKeys': 10,
+		'Prefix': ''
+	}).promise()
+		.then(data => {
+			console.log(data);           // successful response
+		})
+		.catch(err => {
+			console.log(err, err.stack); // an error occurred
+		});
 
 	callback(null, 'Successfully executed');
 
