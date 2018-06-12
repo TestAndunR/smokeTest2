0473f70e7f12c3b0ca76f32d9f2945adeb8f4590
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-06-12 12:11:44 +0530

    remove s3 trigger resource and ses

--- smokeTest2/lambda.js
+++ smokeTest2/lambda.js
@@ -4,39 +4,7 @@ const s3 = new AWS.S3();
 exports.handler = function (event, context, callback) {
 
 	console.log("S3 as a trigger");
-	s3.listObjects({
-		'Bucket': 'sigma.smoketest',
-		'MaxKeys': 10,
-		'Prefix': ''
-	}).promise()
-		.then(data => {
-			console.log(data);           // successful response
-		})
-		.catch(err => {
-			console.log(err, err.stack); // an error occurred
-		});
 
-	ses.sendEmail({
-		Destination: {
-			ToAddresses: ['kannangarar1993@gmail.com'],
-			CcAddresses: [],
-			BccAddresses: []
-		},
-		Message: {
-			Body: {
-				Text: {
-					Data: ''
-				}
-			},
-			Subject: {
-				Data: 'SmokeTest'
-			}
-		},
-		Source: 'andun@adroitlogic.com',
-	}, function (err, data) {
-		if (err) console.log(err, err.stack); // an error occurred
-		else console.log(data);           // successful response
-	});
 
 
 	callback(null, 'Successfully executed');
